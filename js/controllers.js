e94c3af17998b71f0a343c3db777f3f29a3c5d96
lukaszotto <lukasz.otto@yahoo.com>
2014-10-20 21:42:20 +0200

    clear slider timer after changeSlide

--- js/controllers.js
+++ js/controllers.js
@@ -1,29 +1,46 @@
-var sliderApp = angular.module('sliderApp', []);
-
-sliderApp.controller('sliderController', function ($scope, $http, $interval) {
-
-    $http.get('/ng-slider/images.json').success(function (data) {
+var sliderApp, host, rate;
+//globals
+sliderApp = angular.module('sliderApp', []);
+host = 'http://localhost/ng-slider';
+rate = 1000;
+//controller
+sliderApp.controller('sliderController', function ($scope, $http, $timeout) {
+    $http.get(host + '/images.json').success(function (data) {
         $scope.images = data;
-        
-        $scope.currentIndex = 0; // Initially the index is at the first image
+        // Initially the index is at the first image
+        $scope.currentIndex = 0; 
          
         $scope.images.forEach(function(image) {
-            image.visible = false; // make every image invisible
+            // make every image invisible
+            image.visible = false; 
         });     
-        $scope.images[$scope.currentIndex].visible = true; // make the current image visible
+        // make the current image visible
+        $scope.images[$scope.currentIndex].visible = true; 
+        //initial timeout for first slide
+        interval = $timeout(function () {
+            var counter = $scope.currentIndex;
+            counter++;
+            $scope.changeSlide(counter);
+        }, rate);
+        //function to change slide
         $scope.changeSlide = function (slideNumber) {
+            //cancel existing timeout
+            $timeout.cancel(interval);
+            //change slide
             $scope.images[$scope.currentIndex].visible = false;
             $scope.currentIndex = slideNumber;
             $scope.images[slideNumber].visible = true;
+            //after slide changed set new interval for next slide
+            interval = $timeout(function () {
+                var counter = $scope.currentIndex;
+                counter++;
+                if (counter == $scope.images.length) {
+                    counter = 0;
+                }
+                $scope.changeSlide(counter);
+            }, rate);
         };
-        $interval(function () {
-            var counter = $scope.currentIndex;
-            counter++;
-            if (counter == $scope.images.length) {
-                counter = 0;
-            }
-            $scope.changeSlide(counter);
-        }, 3000);
+        
     });
 
 
